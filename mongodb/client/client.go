6aa3d32f2b6be5c2e00739f454dc873fea93cdfa
cenk.ayla <l1511012051@stud.sdu.edu.tr>
2025-07-02 16:09:38 +0300

    add ping and set retrys explicitly for mongo client

--- mongodb/client/client.go
+++ mongodb/client/client.go
@@ -2,8 +2,9 @@ package client
 
 import (
 	"context"
-	config "github.com/Trendyol/go-dcp-mongodb/configs"
 	"time"
+
+	config "github.com/Trendyol/go-dcp-mongodb/configs"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -12,7 +13,11 @@ import (
 func NewMongoClient(cfg config.MongoDB) (*mongo.Client, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
+
 	clientOpts := options.Client().ApplyURI("mongodb://" + cfg.URI)
+	clientOpts.SetRetryWrites(true)
+	clientOpts.SetRetryReads(true)
+
 	if cfg.Username != "" && cfg.Password != "" {
 		clientOpts.SetAuth(options.Credential{
 			Username:   cfg.Username,
@@ -20,9 +25,19 @@ func NewMongoClient(cfg config.MongoDB) (*mongo.Client, error) {
 			AuthSource: cfg.Database,
 		})
 	}
+
 	client, err := mongo.Connect(ctx, clientOpts)
 	if err != nil {
 		return nil, err
 	}
+
+	pingCtx, pingCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer pingCancel()
+
+	if err = client.Ping(pingCtx, nil); err != nil {
+		client.Disconnect(ctx)
+		return nil, err
+	}
+
 	return client, nil
 }
